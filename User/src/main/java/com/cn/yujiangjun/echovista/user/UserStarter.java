d6989ba960f74c2c36dc916235c442ecabbd9b6b
余江俊 <18512106361@163.com>
2023-12-31 10:02:02 +0800

    添加了User模块

---
+++ User/src/main/java/com/cn/yujiangjun/echovista/user/UserStarter.java
@@ -0,0 +1,11 @@
+package com.cn.yujiangjun.echovista.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(UserStarter.class,args);
+    }
+}
