a915738e502050da26e234e0ad8e8513f9f102e0
余江俊 <18512106361@163.com>
2024-01-20 12:46:04 +0800

    登录模块

--- Common/src/main/java/com/cn/yujiangjun/echovista/common/config/RedisConfig.java
+++ Common/src/main/java/com/cn/yujiangjun/echovista/common/config/RedisConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,10 +11,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public LettuceConnectionFactory connectionFactory(){
-        return new LettuceConnectionFactory();
-    }
 
     @Bean
     public  RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory connectionFactory){
